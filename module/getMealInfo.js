f4283b9de7911dab902cf41a7eb04f9c951985a9
kayemba <kayembejonathan5@gmail.com>
2023-04-28 08:40:29 +0200

    fix eslint

--- module/getMealInfo.js
+++ module/getMealInfo.js
@@ -1,12 +1,4 @@
 import countItems from './itemCount.js';
-
-import renderPopup from './popup.js';
-
-const foodCards = document.querySelector('.food-list');
-const meals = [];
-const renderHtmlPage = async (meal) => {
-  await meal.forEach((item) => {
-=======
 import getLikes from './likeCount.js';
 import like from './addLike.js';
 
@@ -14,7 +6,6 @@ const foodCards = document.querySelector('.food-list');
 const meals = [];
 function renderHtmlPage(meal) {
   meal.forEach((item) => {
-
     foodCards.innerHTML += `
     <div class="food-card">
     <img src="${item.meals[0].strMealThumb}" class="img-food">
@@ -28,31 +19,23 @@ function renderHtmlPage(meal) {
       <div class="likes">likes</div>
     </div>
     <div class="button">
-
-    <input type="button" class="btnComments" id="${item.meals[0].idMeal}" value="Comments">
-=======
     <input type="button" class="btnComments" value="Comments">
-
     <input type="button" class="btnReserve" value="Reserve">
     </div>
     </div>
     `;
-
     getLikes();
-
     const btnLike = document.querySelectorAll('.like');
     btnLike.forEach((btn) => {
       btn.addEventListener('click', (e) => {
         const id = Number(e.target.id);
-
-        id();
+        like(id);
       });
     });
   });
-};
+}
 
-
-const displayFoodDetails = async () => {
+const displayFoodDetails = () => {
   const getResponse = async () => {
     for (let i = 52770; i < 52778; i += 1) {
       const objectItem = fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${i}`).then((res) => res.json());
@@ -63,19 +46,6 @@ const displayFoodDetails = async () => {
   getResponse().then((meals) => {
     renderHtmlPage(meals);
   }).then(async () => {
-
-    // Get and display number of like
-    // Get and display number of items
-    const numOfItems = document.querySelector('.number-of-items');
-    countItems(meals.length, numOfItems);
-
-    const btnComment = document.querySelectorAll('.btnComments');
-    btnComment.forEach((element) => {
-      element.addEventListener('click', (e) => {
-        renderPopup(e.target.id);
-      });
-    });
-
     // Get and display number of likes
     getLikes();
     // Get and display number of items
