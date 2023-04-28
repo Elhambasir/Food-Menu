45e461204fe1c6f1e26e8439790bb88270e76297
kayemba <kayembejonathan5@gmail.com>
2023-04-28 08:11:08 +0200

    Add display comment

--- module/getMealInfo.js
+++ module/getMealInfo.js
@@ -1,74 +1,60 @@
 import countItems from './itemCount.js';
-
-const likeApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-const likeApiKey = 'DQ1WY7tbkUIhRnRaIdyZ';
+import renderPopup from './popup.js';
 
 const foodCards = document.querySelector('.food-list');
 const meals = [];
-function renderHtmlPage(meals) {
-  foodCards.innerHTML = meals.map((meal) => `
-  <div class="food-card">
-    <img src="${meal[0].strMealThumb}" class="img-food">
+const renderHtmlPage = async (meal) => {
+  await meal.forEach((item) => {
+    foodCards.innerHTML += `
+    <div class="food-card">
+    <img src="${item.meals[0].strMealThumb}" class="img-food">
     <div class="food-info">
-      <p class="food-name">${meal[0].strMeal}</p>
+      <p class="food-name">${item.meals[0].strMeal}</p>
       </div>
       <div class="like-section">
-      <p class="like-symbol" data-id=${meal[0].idMeal}><i class="fa fa-heart"></i></p>
-      <div class="like-count" data-id=${meal[0].idMeal}>
+      <p class="like-symbol" data-id="${item.meals[0].idMeal}"><i id="${item.meals[0].idMeal}" class=" like fa fa-heart"></i></p>
+      <div class="like-count" data-id="${item.meals[0].idMeal}">
       </div>
       <div class="likes">likes</div>
     </div>
     <div class="button">
-      <input type="button" class="btnComments" value="Comments">
-      <input type="button" class="btnReserve" value="Reserve">
+    <input type="button" class="btnComments" id="${item.meals[0].idMeal}" value="Comments">
+    <input type="button" class="btnReserve" value="Reserve">
     </div>
-  </div>
-`).join('');
-}
+    </div>
+    `;
+    const btnLike = document.querySelectorAll('.like');
+    btnLike.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const id = Number(e.target.id);
+        id();
+      });
+    });
+  });
+};
 
-const displayFoodDetails = () => {
+const displayFoodDetails = async () => {
   const getResponse = async () => {
-    for (let i = 0; i < 9; i += 1) {
-      // eslint-disable-next-line no-await-in-loop
-      const response = await fetch(
-        'https://www.themealdb.com/api/json/v1/1/random.php',
-        {
-          method: 'GET',
-        },
-      );
-      // eslint-disable-next-line no-await-in-loop
-      const foodRandomdata = await response.json();
-      meals.push(foodRandomdata.meals);
-      // eslint-disable-next-line space-infix-ops
+    for (let i = 52770; i < 52778; i += 1) {
+      const objectItem = fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${i}`).then((res) => res.json());
+      meals.push(objectItem);
     }
-    return meals;
+    return Promise.all(meals);
   };
   getResponse().then((meals) => {
     renderHtmlPage(meals);
   }).then(async () => {
+    // Get and display number of like
+    // Get and display number of items
     const numOfItems = document.querySelector('.number-of-items');
     countItems(meals.length, numOfItems);
-    const likeCounterAPI = await fetch(`${likeApi}${likeApiKey}/likes/`)
-      .then((response) => response.json())
-      .then((data) => data);
 
-    const likesCounter = foodCards.querySelectorAll('.like-count');
-    likesCounter.forEach((likeCounter) => {
-      const likesId = likeCounter.getAttribute('data-id');
-      const liked = document.querySelectorAll('.like-symbol');
-      const element = likeCounter;
-      likeCounterAPI.forEach((likeAPI) => {
-        if (likesId === likeAPI.item_id) {
-          liked.forEach((likedItem) => {
-            if (likesId === likedItem.getAttribute('data-id')) {
-              likedItem.classList.add('liked');
-            }
-          });
-          element.innerHTML = likeAPI.likes;
-        }
+    const btnComment = document.querySelectorAll('.btnComments');
+    btnComment.forEach((element) => {
+      element.addEventListener('click', (e) => {
+        renderPopup(e.target.id);
       });
     });
-    // To add or Create like when Heart is pressed
   });
 };
 
--- module/popup.js
+++ module/popup.js
@@ -1,28 +1,64 @@
-const popup = () => {
-  const master = document.querySelector('.container');
-  master.innerHTML = `  <section class="popup" id="popup-close">
- <div class="container">
-        <span class="fa fa-close align-end" id="popup-close"></span>
-        <div class="image"></div>
-        <p class="title">Meals 3</p>
+import getComments from './displayComments.js';
+// eslint-disable-next-line import/no-cycle
+import addComment from './addComment.js';
+
+// function getComment(id, element) {
+//   console.log(element);
+//   const comments = fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/i3eTd09nELlrgd0snSwn/comments?item_id=${id}`)
+//     .then((response) => response.json());
+//   comments.forEach((item) => {
+//     element.innerHTML += `
+//         ${item.creation_date} ${item.username}: ${item.comment}</p> `;
+//   });
+// }
+const getLikesById = async (id) => {
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DQ1WY7tbkUIhRnRaIdyZ/likes')
+    .then((response) => response.json())
+    .then((data) => {
+      // eslint-disable-next-line consistent-return
+      data.forEach((likeCounter) => {
+        if (+likeCounter.item_id === +id) {
+          document.querySelector('.likesNum').innerHTML = `${'Likes : '}${likeCounter.likes}`;
+          getComments(+id, document.querySelector('.comments'));
+        }
+      });
+      const btnSubmit = document.querySelector('.submit');
+      btnSubmit.addEventListener('click', () => { addComment(id); });
+
+      const btnClose = document.querySelector('#btnClose');
+      btnClose.addEventListener('click', () => {
+        const popuptElement = document.querySelector('.popup');
+        popuptElement.setAttribute('style', 'display:none;');
+      });
+    });
+};
+const renderPopup = async (id) => {
+  await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((res) => res.json())
+    .then((d) => {
+      const master = document.querySelector('.container');
+      master.innerHTML = `
+      <span class="fa fa-close align-end" id="btnClose"></span>
+        <div class="image"><img src="${d.meals[0].strMealThumb}" /></div>
+        <p class="title">${d.meals[0].strMeal}</p>
         <div class="names-content" id="names-content">
-          <div class="names">Categories: titanium</div>
-          <div  class="names">Weight: 400</div>
-          <div  class="names">Length: 100,000</div>
-          <div  class="names">Power: 100,000,000</div>
+          <div class="area">Area: ${d.meals[0].strArea} </div>
+          <div  class="likesNum">Likes: ${getLikesById(d.meals[0].idMeal)}</div>
+          <div  class="category">Category: ${d.meals[0].strCategory}</div>
+          <div  class="strTag">Tags: ${d.meals[0].strTags}</div>
         </div>
         <div class="list">
         <h2>Add Comments</h2>
-        <div></div>
-       </div>
-      
-       <form class="form-control" id="Form">
-        <input type="text" class="input" id="input" placeholder="your name">
-        <textarea class="text" name="comment" id="Text" cols="30" rows="10" placeholder="your comment"></textarea>
-        <button type="submit">Add comment</button>
-       </form>
- </div>
-   </section>`;
+        <div class="comments"></div>
+        </div>
+        
+        <form class="form-control" id="Form">
+        <input type="text" class="name" id="input" placeholder="your name">
+        <textarea class="your-comment" name="comment" id="Text" cols="30" rows="5" placeholder="your comment"></textarea>
+        <button class="submit" type="button">Add comment</button>
+        </form>`;
+      const popuptElement = document.querySelector('.popup');
+      popuptElement.setAttribute('style', 'display:block;');
+    });
 };
-
-export default popup;
+export default renderPopup;
